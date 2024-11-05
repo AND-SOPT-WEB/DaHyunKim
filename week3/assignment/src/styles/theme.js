b3afe6b74f0d047f1b25f88b4d892b88f1740e47
daahyunk <ekgusdl0408@naver.com>
2024-11-05 13:39:34 +0900

    feat: 스타일 설정 파일 추가

---
+++ week3/assignment/src/styles/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+    colors: {
+      white: "#FFFFFF",
+      lightGray: "#F2F2F2",
+      gray: "#C4C4C4",
+      darkGray: "#7D7D7D",
+      black: "#000000",
+    },
+    fonts: {
+      logo: "Aftika, sans-serif",
+    },
+  };
+  
+  export default theme;
+  
