d90a37761ad7ee38f2294a436758c6d3eb3de9f0
daahyunk <ekgusdl0408@naver.com>
2024-11-12 15:21:48 +0900

    init: globalStyle, theme 추가

---
+++ week4/assignment/src/styles/theme.ts
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    black: '#000',
+    white: '#ffffff',
+    background: '#f8f9fa',
+    border: '#c1c7cd',
+    primary: '#9d4646',
+    primaryHover: '#ce2939',
+  },
+};
+
+export default theme;
